4766c89db82df3efe2d105e67d5220c2716bac57
kedarnhegde <kedarnhegde@gmail.com>
2020-07-01 16:16:42 +0530

    Add Comments

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -26,9 +26,8 @@ class CommentForm extends Component {
 	}
 
 	handleSubmit(values) {
-		console.log('Current State is: ' + JSON.stringify(values));
-		alert('Current State is: ' + JSON.stringify(values));
 		this.toggleModal();
+		this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
 	}
 	
 	render() {
@@ -99,7 +98,7 @@ function RenderDish({dish}) {
 		);
 }
 
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, dishId}) {
 	if(comments != null) {
 		const commentsList = comments.map((comment) => {
 			return (
@@ -116,7 +115,7 @@ function RenderComments({comments}) {
 				<ul className="list-unstyled">
 					{commentsList}
 				</ul>
-				<CommentForm />
+				<CommentForm dishId={dishId} addComment={addComment} />
 			</div>
 		);
 	} else
@@ -141,7 +140,10 @@ const DishDetail = (props) => {
 				</div>
 				<div className="row">
 					<RenderDish dish={props.dish} />
-					<RenderComments comments={props.comments} />
+					<RenderComments comments={props.comments} 
+					addComment={props.addComment}
+					dishId={props.dish.id}
+					/>
 				</div>
 			</div>
 		);
